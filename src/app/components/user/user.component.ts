b33581360e12efac2b65c3f3c19da85be4cb26e9
YunaH <YunaH503@gmail.com>
2020-08-31 17:48:03 -0400

    Created custom user component with ts, html, css

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+// Decorator
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  // template: '<h2>John Doe</h2>'
+  // styles: [`
+  //   h2 {
+  //     color: blue
+  //   }
+  //   `]
+})
+
+// export makes this class available outside of this file
+export class UserComponent {
+
+}
