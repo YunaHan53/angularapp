52480d5fae487c3cb24201206878485ba3c61adc
YunaH <YunaH503@gmail.com>
2020-08-31 23:12:34 -0400

    Updated user component to use ngOnInit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { User } from '../../models/User'
 // Decorator
@@ -9,12 +9,16 @@ import { User } from '../../models/User'
 })
 
 // export makes this class available outside of this file
-export class UserComponent {
+export class UserComponent implements OnInit {
   // Properties
   user: User;
 
   // Methods
   constructor() {
+
+  }
+
+  ngOnInit() {
     this.user = {
       firstName: 'John',
       lastName: 'Doe',
