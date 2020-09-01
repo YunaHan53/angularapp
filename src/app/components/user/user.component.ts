217637c5043464fc4d95fe25f57b4caf22851fea
YunaH <YunaH503@gmail.com>
2020-08-31 21:32:14 -0400

    Practiced with properties and methods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -4,16 +4,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  // template: '<h2>John Doe</h2>'
-  // styles: [`
-  //   h2 {
-  //     color: blue
-  //   }
-  //   `]
+  styleUrls: ['./user.component.css']
 })
 
 // export makes this class available outside of this file
 export class UserComponent {
+  // Properties
+  firstName = 'John';
+  lastName = 'Doe';
+  age = 30;
 
+  // Methods
+  constructor() {
+    this.sayHello(); // Hello John
+    console.log(this.age); // 30
+    this.hasBirthday();
+    console.log(this.age); // 31
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`)
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
 }
