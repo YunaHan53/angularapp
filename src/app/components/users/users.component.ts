e81787b538de3e89b7ad26a36ca6d2a071b67339
YunaH <YunaH503@gmail.com>
2020-09-01 17:22:33 -0400

    Practiced using ngIf to set conditionals for Users
    Added if statement to showExtended if there are users in the array
    If there are no users found in the Users array, display 'No Users Found'
    Set the age and address in the User.ts to be optional.
    If there are no age and address included, do not showExtended.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,55 +9,55 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  // If fetching data from an outside source
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-          street: '50 Main st',
-          city: 'Boston',
-          state: 'MA'
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 34,
+          address: {
+            street: '20 School st',
+            city: 'Lynn',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'FL'
+          }
         }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Johnson',
-        age: 34,
-        address: {
-          street: '20 School st',
-          city: 'Lynn',
-          state: 'MA'
-        }
-      },
-      {
-        firstName: 'Karen',
-        lastName: 'Williams',
-        age: 26,
-        address: {
-          street: '55 Mill st',
-          city: 'Miami',
-          state: 'FL'
-        }
-      }
-    ];
+      ];
+
+      this.loaded = true;
 
     // Calling the addUser method and passing in a new user
     this.addUser({
       firstName: 'David',
-      lastName: 'Jackson',
-      age: 26,
-      address: {
-        street: '12 Wake st',
-        city: 'Miami',
-        state: 'FL'
-      }
+      lastName: 'Jackson'
     });
   }
+
 
   // addUser method
   addUser(user: User) {
